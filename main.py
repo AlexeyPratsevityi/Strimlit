ed678f5495988cded412a829426eb31c97256f35
AlexeyPratsevityi <alexeyprats@gmail.com>
2023-03-07 15:25:16 +0300

    данными о котировках компании Apple

---
+++ main.py
@@ -0,0 +1,21 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+import matplotlib.pyplot as plt
+import mplfinance as mpf
+
+st.write("""
+#  Японские свечная диаграма для Apple Inc. Цена на акции
+""")
+tickerSymbol = 'AAPL'
+tickerData = yf.Ticker(tickerSymbol)
+tickerDf = tickerData.history(period='1m',interval='1mo', start='2022-01-30')
+tickerDf = tickerDf[['Open', 'High', 'Low', 'Close', 'Volume']]
+fig, ax = mpf.plot(tickerDf, type='candle', volume=True, returnfig=True)
+st.pyplot(fig)
+
+
+
+
+
+
